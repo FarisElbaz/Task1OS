8b18bf304aeb7526e29feb74f7d956a58bd2669e
LilWenis <120251539+FarisElbaz@users.noreply.github.com>
2025-02-05 11:21:59 +0200

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,5 +1,4 @@
 #include <pthread.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,7 +17,7 @@ typedef struct {
     int segment_size;
     WordCount **word_counts;
     pthread_mutex_t *mutex;
-} ThreadData; 
+} ThreadData;
 
 void *count_words(void *arg) {
     ThreadData *data = (ThreadData *)arg;
@@ -29,6 +28,7 @@ void *count_words(void *arg) {
 
     char word[MAX_WORD_LENGTH];
     int index = 0;
+
     for (int i = 0; i < segment_size; i++) {
         if (isalpha(segment[i])) {
             word[index++] = tolower(segment[i]);
@@ -52,6 +52,7 @@ void *count_words(void *arg) {
             index = 0;
         }
     }
+
     if (index > 0) {
         word[index] = '\0';
         pthread_mutex_lock(mutex);
@@ -70,7 +71,19 @@ void *count_words(void *arg) {
         }
         pthread_mutex_unlock(mutex);
     }
+
     return NULL;
+}
+
+void adjust_segment_boundaries(char *text, int *start, int *end, long file_size) {
+    // Move start backwards if it’s in the middle of a word
+    while (*start > 0 && isalpha(text[*start])) {
+        (*start)--;
+    }
+    // Move end forward if it’s in the middle of a word
+    while (*end < file_size && isalpha(text[*end])) {
+        (*end)++;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -103,12 +116,21 @@ int main(int argc, char *argv[]) {
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
     int segment_size = file_size / num_threads;
+
     for (int i = 0; i < num_threads; i++) {
-        thread_data[i].segment = text + i * segment_size;
-        thread_data[i].segment_size = (i == num_threads - 1) ? file_size - i * segment_size : segment_size;
+        int start = i * segment_size;
+        int end = (i == num_threads - 1) ? file_size : start + segment_size;
+
+        adjust_segment_boundaries(text, &start, &end, file_size);
+        
+        thread_data[i].segment = text + start;
+        thread_data[i].segment_size = end - start;
         thread_data[i].word_counts = &word_counts;
         thread_data[i].mutex = &mutex;
+
         pthread_create(&threads[i], NULL, count_words, &thread_data[i]);
+
+        printf("Thread %d: Start=%d, End=%d\n", i, start, end);
     }
 
     for (int i = 0; i < num_threads; i++) {
